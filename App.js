763c489baa5e79a47bc3ae5d958fc0773672d9e1
LiteHarvey <harveycayetano@gmail.com>
2021-02-05 22:43:59 +0300

    updated on the alert message

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    alert(`Bar code was scanned ${type} and data ${data} has been scanned!`);
   };
 
   if (hasPermission === null) {
